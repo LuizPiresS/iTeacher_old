a42b2ed8568d01129bcbb7689d0dc29a21e25b62
Luiz Pires <kw13code@gmail.com>
2020-05-02 21:52:13 -0300

    test: garante que AddAccountTeacherController retorne 400 o campo name esteja em branco

---
+++ src/presentation/controllers/account-teacher/add-account-teacher.spec.ts
@@ -0,0 +1,50 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+// import { HttpRequest } from '../../protocols/http'
+import { AddAccountTeacherController } from './add-account-teacher'
+
+// const makeHttpRequest = (): HttpRequest => ({
+//   body: {
+//     name: 'any_name',
+//     cpf: 'any_cpf',
+//     birthDate: 'any_birthDate',
+//     email: 'any_mail@mail.com',
+//     cellphone: 'any_cellphone',
+//     whatsApp: 'any_whatsApp',
+//     photo: 'any_photo',
+//     lattes: 'any_lattes',
+//     about: 'any_about',
+//     password: 'any_password'
+//   }
+// })
+
+type SutTypes = {
+  sut: AddAccountTeacherController
+}
+const makeSut = (): SutTypes => {
+  const sut = new AddAccountTeacherController()
+  return {
+    sut
+  }
+}
+
+describe('AddAccountTeacher Controller', () => {
+  test('Espero que retorne 400 o campo name esteja em branco', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        cpf: 'any_cpf',
+        birthDate: 'any_birthDate',
+        email: 'any_mail@mail.com',
+        cellphone: 'any_cellphone',
+        whatsApp: 'any_whatsApp',
+        photo: 'any_photo',
+        lattes: 'any_lattes',
+        about: 'any_about',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toEqual(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  })
+})
