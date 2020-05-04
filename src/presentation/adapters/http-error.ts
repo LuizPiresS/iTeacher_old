b78a3c554c18fae6e2863e9733a87cc6d7611992
Luiz Pires <kw13code@gmail.com>
2020-05-03 21:14:09 -0300

    feat: adicionando o erro server-error

--- src/presentation/adapters/http-error.ts
+++ src/presentation/adapters/http-error.ts
@@ -1,3 +1,4 @@
+import { ServerError } from '../errors/server-error'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => {
@@ -11,5 +12,12 @@ export const duplicatedFieldError = (error: Error): HttpResponse => {
   return {
     statusCode: 400,
     body: error
+  }
+}
+
+export const serverError = (error: Error): HttpResponse => {
+  return {
+    statusCode: 500,
+    body: new ServerError(error.stack)
   }
 }
