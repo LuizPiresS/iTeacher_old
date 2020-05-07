b52055e93951b03f64d332cb78585a2b26b83cc1
Luiz Pires <kw13code@gmail.com>
2020-05-06 21:21:50 -0300

    refactor: removendo arquivo

--- src/data/protocols/add-account-teacher-repository.ts
+++
@@ -1,6 +0,0 @@
-import { AddAccountTeacherModel } from '@/domain/models/account-teacher/add-account-teacher-model'
-import { AddAccountTeacherParams } from '@/domain/usecases/account-teacher/add-account'
-
-export interface AddAccountTeacherRepository {
-  add(account: AddAccountTeacherParams): Promise<AddAccountTeacherModel>
-}
