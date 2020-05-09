4aaf2c329e6f19b1a81cecdd56bc0803998eef27
Luiz Pires <kw13code@gmail.com>
2020-05-09 03:32:26 -0300

    refactor: mudando o retorno de AddAccountTeacherRepository

--- src/data/protocols/database/add-account-teacher/add-account-teacher-repository.ts
+++ src/data/protocols/database/add-account-teacher/add-account-teacher-repository.ts
@@ -1,6 +1,5 @@
-import { AddAccountTeacherModel } from '@/domain/models/account-teacher/add-account-teacher-model'
 import { AddAccountTeacherParams } from '@/domain/usecases/account-teacher/add-account-teacher'
 
 export interface AddAccountTeacherRepository {
-  add (accountData: AddAccountTeacherParams): Promise<AddAccountTeacherModel>
+  add (accountData: AddAccountTeacherParams): Promise<boolean>
 }
