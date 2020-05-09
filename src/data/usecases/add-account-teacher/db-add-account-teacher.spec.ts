d8119a35d842160319f5f2ef22b2878c992c2884
Luiz Pires <kw13code@gmail.com>
2020-05-09 03:34:26 -0300

    test: garante que DbAddAccount envie um email de verificação

--- src/data/usecases/add-account-teacher/db-add-account-teacher.spec.ts
+++ src/data/usecases/add-account-teacher/db-add-account-teacher.spec.ts
@@ -1,19 +1,8 @@
-import { AddAccountTeacherModel } from '@/domain/models/account-teacher/add-account-teacher-model'
-
 import { Hasher } from '../../protocols/criptograpy/hasher'
 import { AddAccountTeacherRepository } from '../../protocols/database/add-account-teacher/add-account-teacher-repository'
 import { SendEmail } from '../../protocols/email/send-email'
+import { Uuid } from '../../protocols/uuid/uuid'
 import { DbAddAccountTeacher, AddAccountTeacherParams } from './db-add-account-protocols'
-
-//  Mock do retorno final da classe
-const mockAccountModel = (): AddAccountTeacherModel => ({
-  id: 1,
-  uuid: 'any_uuid',
-  name: 'any_name',
-  email: 'any_mail@mail.com',
-  password: 'hashed_password',
-  token: 'any_token'
-})
 
 const mockSendEmail = (): SendEmail => {
   class SendEmailStub implements SendEmail {
@@ -35,8 +24,8 @@ const mockAccount = (): AddAccountTeacherParams => (
 
 const mockAddAccountTeacherRepository = (): AddAccountTeacherRepository => {
   class AddAccountTeacherRepositoryStub implements AddAccountTeacherRepository {
-    async add (accountData: AddAccountTeacherParams): Promise<AddAccountTeacherModel> {
-      return await mockAccountModel()
+    async add (accountData: AddAccountTeacherParams): Promise<boolean> {
+      return Promise.resolve(true)
     }
   }
 
@@ -52,25 +41,37 @@ const mockHasher = (): Hasher => {
   return new HasherStub()
 }
 
+const mockUuid = (): Uuid => {
+  class UuidStub implements Uuid {
+    generator (): string {
+      return 'any_uuid'
+    }
+  }
+  return new UuidStub()
+}
+
 type SutTypes = {
   sut: DbAddAccountTeacher
   hasherStub: Hasher
   addAccountTeacherRepositoryStub: AddAccountTeacherRepository
   sendEmailStub: SendEmail
+  uuidStub: Uuid
 }
 const makeSut = (): SutTypes => {
   const hasherStub = mockHasher()
   const addAccountTeacherRepositoryStub = mockAddAccountTeacherRepository()
   const sendEmailStub = mockSendEmail()
+  const uuidStub = mockUuid()
   const sut = new DbAddAccountTeacher(hasherStub, addAccountTeacherRepositoryStub, sendEmailStub)
   return {
     sut,
     hasherStub,
     addAccountTeacherRepositoryStub,
-    sendEmailStub
+    sendEmailStub,
+    uuidStub
   }
 }
-describe('AddAccountTeacher', () => {
+describe('DbAddAccountTeacher', () => {
   test('Espero que o Encryper seja chamado apenas com o valor correto', async () => {
     const { sut, hasherStub } = makeSut()
 
@@ -93,7 +94,7 @@ describe('AddAccountTeacher', () => {
     expect(addAccountTeacherRepositorySpy).toHaveBeenCalledWith(account)
   })
 
-  test('Espero que retorne 500 caso Hasher retorne uma excpetion', async () => {
+  test('Espero que lance uma exception caso Hasher retorne uma excpetion', async () => {
     const { sut, hasherStub } = makeSut()
     jest.spyOn(hasherStub, 'hash').mockReturnValueOnce(Promise.reject(new Error()))
     const promise = sut.add(mockAccount())
@@ -101,11 +102,42 @@ describe('AddAccountTeacher', () => {
     await expect(promise).rejects.toThrow()
   })
 
-  test('Espero que retorne 500 caso AddAccountTeacherRepository retorne uma excpetion', async () => {
+  test('Espero que lance uma exception caso AddAccountTeacherRepository retorne uma excpetion', async () => {
     const { sut, addAccountTeacherRepositoryStub } = makeSut()
     jest.spyOn(addAccountTeacherRepositoryStub, 'add').mockReturnValueOnce(Promise.reject(new Error()))
     const promise = sut.add(mockAccount())
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Espero que lance uma exception caso SendEmail retorne uma excpetion', async () => {
+    const { sut, sendEmailStub } = makeSut()
+    jest.spyOn(sendEmailStub, 'send').mockReturnValueOnce(Promise.reject(new Error()))
+    const promise = sut.add(mockAccount())
+
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Espero que AddAccountTeacherRepository retorne false caso não seja possivel salvar a nova conta ', async () => {
+    const { sut, addAccountTeacherRepositoryStub } = makeSut()
+    jest.spyOn(addAccountTeacherRepositoryStub, 'add').mockReturnValueOnce(Promise.resolve(false))
+    const response = await sut.add(mockAccount())
+
+    expect(response).toBeFalsy()
+  })
+
+  test('Espero que AddAccountTeacherRepository retorne false caso não seja possivel enviar o email ', async () => {
+    const { sut, sendEmailStub } = makeSut()
+    jest.spyOn(sendEmailStub, 'send').mockReturnValueOnce(Promise.resolve(false))
+    const response = await sut.add(mockAccount())
+
+    expect(response).toBeFalsy()
+  })
+
+  test('Espero que AddAccountTeacherRepository retorne true caso a nova conta seja salva e o e-mail de verificação seja enviado', async () => {
+    const { sut } = makeSut()
+    const response = await sut.add(mockAccount())
+
+    expect(response).toBeTruthy()
   })
 })
