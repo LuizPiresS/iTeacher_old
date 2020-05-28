b1442e736ff3a25dcaf159c6c6e2c2780b27ea7c
Luiz Pires <kw13code@gmail.com>
2020-05-28 04:20:52 +0000

    chore: add config files

---
+++ src/config/app.config.ts
@@ -0,0 +1,3 @@
+export const appConfig = {
+  domain: 'iteacher.test',
+};
---
+++ src/config/email.config.ts
@@ -0,0 +1,5 @@
+export const emailConfig = {
+  pathEmailTemplate: '/src/email/templates/add-user.ejs',
+  emailFrom: 'emailFrom@maiil.com',
+  key: 'key serviço de e-mail', //TODO! Deixar a key em um .env
+};
