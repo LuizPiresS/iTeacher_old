adc4e5766e74857992cd55cdb1de8bdc15969bda
Luiz Pires <devranzinza@gmail.com>
2020-06-04 20:48:10 +0000

    refactor: remove config files

--- src/config/email.config.ts
+++
@@ -1,5 +0,0 @@
-export const emailConfig = {
-  pathEmailTemplate: '/src/email/templates/add-user.ejs',
-  emailFrom: 'emailFrom@maiil.com',
-  key: 'key serviço de e-mail', //TODO! Deixar a key em um .env
-};
