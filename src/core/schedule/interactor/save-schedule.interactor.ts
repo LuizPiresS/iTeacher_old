017594d2dbbc718542d7e97a9eaeb9376d1a7190
Luiz Pires <kw13code@gmail.com>
2020-06-11 23:19:26 +0000

    fix: error message

--- src/core/schedule/interactor/save-schedule.interactor.ts
+++ src/core/schedule/interactor/save-schedule.interactor.ts
@@ -13,10 +13,10 @@ export class SaveScheduleInteractor {
   async execute(data: ScheduleRequest): Promise<void> {
     try {
       if (data.title === '') {
-        throw new ScheduleTitleInvalidError('Empty field');
+        throw new ScheduleTitleInvalidError('Empty title field');
       }
       if (data.description === '') {
-        throw new ScheduleDescriptionInvalidError('Empty field');
+        throw new ScheduleDescriptionInvalidError('Empty description field');
       }
       // Data persistence
       const {
