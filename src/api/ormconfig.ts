c2329e22dc809a83c11423261b0f77aa2f177c1a
Luiz Pires <kw13code@gmail.com>
2020-08-04 03:50:27 +0000

    refactor: config to use postgres and ts-node-dev

--- src/api/ormconfig.ts
+++ src/api/ormconfig.ts
@@ -1,19 +1,19 @@
-import dotenv from 'dotenv';
 import path from 'path';
 import { ConnectionOptions } from 'typeorm';
 
 const DatabaseDataSourceDirectory = path.resolve(__dirname, '..', 'database');
-dotenv.config();
 
 export default {
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
-  entities: [path.join(DatabaseDataSourceDirectory, 'entities', '*.entity.js')],
+  entities: [
+    path.join(DatabaseDataSourceDirectory, 'entities', '*.entity.{js,ts}'),
+  ],
   migrations: [path.join(DatabaseDataSourceDirectory, 'migrations', '*.js')],
   subscribers: [path.join(DatabaseDataSourceDirectory, 'subscribers', '*.js')],
 } as ConnectionOptions;
