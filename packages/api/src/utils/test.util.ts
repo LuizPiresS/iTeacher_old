9fe76d089eff60ecd17bce05e247e5e6322f5558
Luiz Pires <kw13code@gmail.com>
2020-08-23 08:06:24 +0000

    refactor: remove unnecessary file

--- packages/api/src/utils/test.util.ts
+++
@@ -1,9 +0,0 @@
-export function defineNow(value: string) {
-  jest
-    .spyOn(global.Date, 'now')
-    .mockImplementation(() => new Date(value).valueOf())
-}
-
-export function getNowISO() {
-  return new Date(Date.now()).toISOString()
-}
