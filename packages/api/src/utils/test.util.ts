07b0e09c04fb37dbbce3c3b80f6fae546c409871
Luiz Pires <kw13code@gmail.com>
2020-08-27 15:16:33 +0000

    build: packages upgrade

---
+++ packages/api/src/utils/test.util.ts
@@ -0,0 +1,9 @@
+export function defineNow(value: string): void {
+  jest
+    .spyOn(global.Date, 'now')
+    .mockImplementation(() => new Date(value).valueOf())
+}
+
+export function getNowISO(): string {
+  return new Date(Date.now()).toISOString()
+}
