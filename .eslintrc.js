7bedd2ec983088c1f320c080c1c8683b0a394a1e
Luiz Pires <kw13code@gmail.com>
2020-10-03 22:12:10 -0300

    refactor: add eslint standard code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
-const config = require('@iteacher/eslint-config');
-
-module.exports = config;
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
